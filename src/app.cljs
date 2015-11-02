cdeff2c977d0fe1eaa57760fe5b46f767a632f26
Ruslan Shestopalyuk <rshestopalyuk@slb.com>
2015-11-02 22:33:45 +0100

    - user interaction

--- src/app.cljs
+++ src/app.cljs
@@ -3,7 +3,9 @@
                       :refer [go go-loop]])
     (:require [cljs.core.async :as async
                :refer [>! <! timeout]]
+              [clojure.string :as str]
               [goog.dom :as dom]
+              [goog.dom.classes :as classes]
               [goog.events :as events]
               [goog.style :as style]
               [goog.array]
@@ -15,29 +17,16 @@
 
 (def ^:dynamic *board*)
 (def ^:dynamic *nmoves* 0)
-
-(def b3x3 [1 6 0
-           8 4 2
-           7 5 3])
-
-(def b4x4 [6  14 3  13
-           7  1  0  5
-           8  10 2  12
-           15 9  11 4 ])
-
-(def b6x6 [1   10  25  33  12  19
-           14  2   34  26  17  6
-           18  9   31  4   30  16
-           28  3   21  22  35  24
-           13  32  7    0  5   23
-           27  8   20  11  29  15])
+(def ^:dynamic *solving* false)
 
 (defn put-block [block x y]
-  (style/setStyle block (clj->js
-                         {:left (str (* x block-width) "px")
-                          :top  (str (* y block-width) "px")})))
+  (style/setStyle
+   block (clj->js
+          {:left (str (* x block-width) "px")
+           :top  (str (* y block-width) "px")})))
 
-(defn transition [duration f]
+(defn transition
+  [duration f]
   (let [n      (* duration fps)
         dt     (/ 1000 fps)]
     (go-loop [i 0]
@@ -45,7 +34,10 @@
       (<! (timeout dt))
       (if (< i n) (recur (inc i))))))
 
-(defn lerp [a b t] (+ (* t b) (* (- 1 t) a)))
+(defn lerp
+  "Linear interpolation, y = (1 - t)a + tb"
+  [a b t]
+  (+ (* t b) (* (- 1 t) a)))
 
 (defn get-block-elem [num]
   (->> "board"
@@ -54,60 +46,97 @@
        goog.array/toArray
        (dom/getElement (str num))))
 
-(defn mask-left-top
-  "Returns 'masking function' ...
-  "
-  [[px py]]
-  (fn [[x y]]
-    (and (>= y py) (>= x px))))
-
-(defn play-move [n num]
+(defn play-move [num]
   (let [block    (get-block-elem num)
-        from     (npuzzle/get-idx *board* num)
-        to       (npuzzle/get-idx *board* 0)
-        [x0 y0]  (npuzzle/get-pos n from)
-        [x1 y1]  (npuzzle/get-pos n to)]
-  (if (npuzzle/neighbor? [x0 y0] [x1 y1]) (do
-    (set! *board* (npuzzle/move *board* from to))
-    (set! *nmoves* (inc *nmoves*))
-    (dom/setTextContent (dom/getElement "nmoves") (str *nmoves*))
-    (transition
-     0.1 #(put-block block (lerp x0 x1 %) (lerp y0 y1 %)))))))
+        [x0 y0]  (npuzzle/get-num-pos *board* num)
+        [x1 y1]  (*board* :blank-pos)
+        dmoves   (dom/getElement "moves")]
+  (if (npuzzle/neighbors? [x0 y0] [x1 y1])
+    (do
+      (set! *board* (npuzzle/move *board* [x0 y0]))
+      (set! *nmoves* (inc *nmoves*))
+      (dom/setTextContent
+       dmoves (str (dom/getTextContent dmoves) " " num))
+      (dom/setTextContent (dom/getElement "nmoves") (str *nmoves*))
+      (transition
+       0.1 #(put-block block (lerp x0 x1 %) (lerp y0 y1 %)))))))
 
-(defn play-moves [n moves]
+(defn play-moves [moves]
   (go-loop [i 0]
           (when (< i (count moves))
-                (<! (play-move n (nth moves i)))
-                (recur (inc i)))))
+                (<! (play-move (nth moves i)))
+                (if *solving* (recur (inc i))))))
 
-(defn click-block [n num]
-  ;(play-moves n (npuzzle/solve n *board*)))
-  (play-moves n (npuzzle/move-piece
-                 n *board* (npuzzle/get-pos n (dec num)) num)))
-  ;(play-move n num))
+(defn solve-board
+  []
+  (do
+    (set! *solving* (not *solving*))
+    ((if *solving* classes/add classes/remove)
+     (dom/getElement "solve") "btn_sel")
+    (if *solving* (play-moves (npuzzle/solve-nums *board*)))))
 
+(defn onclick
+  [elem callback]
+  (.listen goog.events elem
+           goog.events.EventType.CLICK callback))
 
-(defn make-block [n idx num]
+(defn make-block [idx num]
   (let [nstr  (str num)
         cl    (if (> num 0) "block" "hidden")
         block (dom/createDom
               "div" (clj->js {:class cl :id nstr}) nstr)
-        [x y] (npuzzle/get-pos n idx)]
+        [x y] (npuzzle/idx->pos *board* idx)]
     (put-block block x y)
-    (.listen goog.events block
-             goog.events.EventType.CLICK
-             #(click-block n num))
+    (onclick block #(if (not *solving*) (play-move num)))
     block))
 
-
-(defn make-board [n cells]
-  (let [s (str (+ (* n block-width) block-margin) "px")
+(defn make-board-control [board]
+  (let [n (*board* :n)
+        s (str (+ (* n block-width) block-margin) "px")
         board  (dom/getElement "board")
-        blocks (map-indexed (partial make-block n) cells)]
+        blocks (map-indexed make-block (*board* :cells))]
     (dom/removeChildren board)
     (style/setStyle board (clj->js {:width s :height s}))
     (doseq [b blocks] (dom/append board b))))
 
-(set! *board* b6x6)
-(set! (.-onload js/window) #(make-board 6 *board*))
+(defn get-url-params
+  "Returns a map of URL-encoded parameters"
+  []
+  (let [url      js/window.location.href
+        [_ par]  (str/split url "?")
+        params   (str/split par "&")]
+    (into {} (map #(str/split % "=") params))))
+
+(def preset-boards {
+  3 [1 6 0
+     8 4 2
+     7 5 3]
+  4 [6  14 3  13
+     7  1  0  5
+     8  10 2  12
+     15 9  11 4 ]
+  6 [1   10  25  33  12  19
+     14  2   34  26  17  6
+     18  9   31  4   30  16
+     28  3   21  22  35  24
+     13  32  7    0  5   23
+     27  8   20  11  29  15] })
+
+(defn parse-nums
+  [nums]
+  (map js/parseInt (str/split nums ",")))
+
+(defn init-board
+  [params]
+  (let [n     (js/parseInt (get params "n" "6"))
+        nums  (params "nums")
+        cells (do (js/console.log (str n " " nums))
+                (if nums (parse-nums nums) (preset-boards n)))]
+    (set! *board* (npuzzle/make-board cells))))
+
+(set! (.-onload js/window)
+      #(do
+         (init-board (get-url-params))
+         (onclick (dom/getElement "solve") solve-board)
+         (make-board-control *board*)))
 
--- src/npuzzle.cljs
+++ src/npuzzle.cljs
@@ -1,8 +1,115 @@
-(ns npuzzle
-  (:require [tailrecursion.priority-map
-             :refer [priority-map priority-map-by]]))
+(ns npuzzle)
 
-;; ## Constants
+;; ## Data structures
+(defrecord Board [
+  cells        ; list of cell numbers, row-major, 0 stands for blank
+  n            ; width of the board
+  m            ; height of the board
+  blank-pos])  ; cached blank position, [x, y]
+
+(defn dist2
+  "Squared Euclidian distance between points `[x0 y0]` and `[x1 y1]`"
+  [[x0 y0] [x1 y1]]
+  (let [dx (- x0 x1)
+        dy (- y0 y1)]
+    (+ (* dx dx) (* dy dy))))
+
+(defn neighbors?
+  "Returns true if points `p0` and `p1` are neighboring
+  (vertically or horizontally) on the board"
+  [p0 p1]
+  (= 1 (dist2 p0 p1)))
+
+;; ## Board utilities
+(defn valid-pos?
+  "Returns true if `[x y]` is a position inside the `board` boundaries"
+  [board [x y]]
+  (and (>= x 0) (< x (board :n))
+    (>= y 0) (< y (board :m))))
+
+(defn pos->idx
+  "Converts `[x y]` board position to the cell index (row-major)"
+  [board [x y]]
+  {:pre [(valid-pos? board [x y])]}
+  (+ x (* (board :n) y)))
+
+(defn idx->pos
+  "Converts cell index `idx` (row major) to board position `[x y]`"
+  [board idx]
+  {:pre  [(>= idx 0) (< idx (* (board :n) (board :m)))]}
+  [(mod idx (board :n)) (quot idx (board :n))])
+
+(defn get-num-idx
+  "Finds number's index in the board cell array"
+  [board num]
+  {:pre [(>= num 0) (<= num (* (board :n) (board :m)))]}
+  (.indexOf (to-array (board :cells)) num))
+
+(defn get-num-pos
+  "Finds number's position in the board cell array"
+  [board num]
+  {:pre [(>= num 0) (<= num (* (board :n) (board :m)))]}
+  (idx->pos board (get-num-idx board num)))
+
+(defn get-num-at
+  "Returns number at the board's position `pos`"
+  [board pos]
+  {:pre [(valid-pos? board pos)]}
+  ((board :cells) (pos->idx board pos)))
+
+(defn find-blank-pos
+  "Returns the position of blank square on the `board`"
+  [board]
+  (get-num-pos board 0))
+
+(defn swap
+  "Returns a pair with swapped elements, `[y, x]`"
+  [[x y]]
+  [y x])
+
+(defn transpose-board
+  "Returns a board with swapped horizontal/vertical dimensions"
+  [{m :m n :n blank-pos :blank-pos cells :cells}]
+  {:blank-pos (swap blank-pos) :n m :m n
+   :cells (->> cells
+            (partition n)
+            (apply mapv vector)
+            (apply concat)
+            vec)})
+
+(defn make-board
+  "Creates board from the array of `cells`
+  (numbers, row-major, 0 is for blank), of width `n`"
+  ([cells] (make-board cells (int (Math/sqrt (count cells)))))
+  ([cells n]
+    {:post [(= (* (% :n) (% :m)) (count cells))]}
+    (let [board {:cells (vec cells)
+                 :m     (quot (count cells) n)
+                 :n     n}]
+      (assoc board :blank-pos (find-blank-pos board)))))
+
+(defn blank?
+  "Returns true if position `pos` in the `board` contains a blank"
+  [board pos]
+  (and (valid-pos? board pos)
+       (= 0 ((board :cells) (pos->idx board pos)))))
+
+(defn move
+  "Moves a number at `from-pos` on the `board` to the blank location
+  (or, another way of seeing it, the blank moves to the `from-pos` location)"
+  [board from-pos]
+  {:pre [(neighbors? from-pos (board :blank-pos))]
+   :post [(blank? % (% :blank-pos))
+          (= (find-blank-pos %) (% :blank-pos))]}
+  (let [to-idx    (pos->idx board (board :blank-pos))
+        from-idx  (pos->idx board from-pos)
+        cells     (board :cells)]
+    (assoc board
+      :cells (assoc cells
+               from-idx (cells to-idx)
+               to-idx   (cells from-idx))
+      :blank-pos from-pos)))
+
 ;;  Directions, in which a piece can move (left, up, right, down)
 (def dir-l [-1  0])
 (def dir-u [ 0 -1])
@@ -10,134 +117,163 @@
 (def dir-d [ 0  1])
 (def directions [dir-l dir-u dir-r dir-d])
 
-;; ![alt text](board.png)
-(defn move [board from to]
-  (assoc board
-    from (board to)
-    to (board from)))
-
-(defn get-idx
-  [board num]
-  (.indexOf (to-array board) num))
-
-(defn get-pos
-  [n idx]
-  [(mod idx n) (quot idx n)])
-
-(defn pos->idx
-  [n [x y]]
-  (+ x (* n y)))
-
-(defn get-num
-  [n board p]
-  (board (pos->idx n p)))
-
-(defn get-num-pos
-  [n board num]
-  (get-pos n (get-idx board num)))
-
-(defn get-blank-pos
-  [n board]
-  (get-num-pos n board 0))
-
-(defn dist2
-  "
-  "
-  [[x0 y0] [x1 y1]]
-  (let [dx (- x0 x1)
-        dy (- y0 y1)]
-    (+ (* dx dx) (* dy dy))))
-
-(defn manhattan-dist
-  "
-  "
-  [[x0 y0] [x1 y1]]
-  (+ (Math/abs (- x0 x1)) (Math/abs (- y0 y1))))
-
-(defn neighbor?
-  [p0 p1]
-  (= 1 (manhattan-dist p0 p1)))
-
-(defn valid-cell?
-  [n [x y]]
-  (and (>= x 0) (< x n) (>= y 0) (< y n)))
-
-(defn top-left-edge?
-  "Returns true if position [`x`, `y`] is in the masked left-top area,
-  (according to the board filling rule) relative to the `pivot` position"
-  [pivot [x y]]
-  (let [[px py] pivot
-        vert?   (> py px)]
-    (if vert?
-      (or (< x px) (<= y px) (and (= x px) (< y py)))
-      (or (< y py) (<  x py) (and (= y py) (< x px))))))
-
 (defn neighbors
-  [n [x y]]
+  "Returns list of valid neighbor positions on the `board` relative to `[x y]`"
+  [board [x y]]
   (->> directions
-       (map (fn [[dx dy]] [(+ x dx) (+ y dy)]))
-       (filter (partial valid-cell? n))))
+    (map (fn [[dx dy]] [(+ x dx) (+ y dy)]))
+    (filter (partial valid-pos? board))))
 
-(defn moves
-  "Finds a sequence of moves to get from position `p0` to position `p1`.
-  `can-move` is an optional predicate for the allowed cells
-  "
-  ([n p0 p1] (moves n p0 p1 (constantly true)))
-  ([n p0 p1 can-move]
-   (loop [res []
-          pc p0]
-     (if (or (= pc p1) (> (count res) 10)) res
-      (let [cm #(and (can-move %) (not (some #{%} res)) (not= % p0))
-            nb (filter cm (neighbors n pc))
-            p  (apply min-key (partial dist2 p1) nb)]
-        (recur (conj res p) p))))))
+(defn num-solved?
+  "Returns true if number at `idx` is at correct (solved) place on the `board"
+  [{cells :cells} idx]
+  {:pre [(>= idx 0) (< idx (count cells))]}
+  (let [num (cells idx)]
+    (= (inc idx)
+      (if (zero? num) (count cells) num))))
 
-(defn move-blank-to
-  [n board p1]
-  (let [p0 (get-blank-pos n board)
-        m  (moves n p0 p1)]
-    (map (partial get-num n board) m)))
+(defn board-solved?
+  "Returns true if the `board` is solved"
+  [board]
+  (every? (partial num-solved? board)
+    (range 0 (count (board :cells)))))
+
+(defn cells-valid?
+  "Returns true if the `board` is a valid n-puzzle board"
+  [cells]
+  (let [nc (count cells)]
+    (= (set (range 0 nc)) (set cells))))
+
+(defn path
+  "Finds a sequence of steps (positions) to get from position `p0` to position `p1`.
+  `can-move?` is an optional predicate for the allowed cells.
+  The path is expected to exist."
+  ([board p0 p1] (path board p0 p1 (constantly true)))
+  ([board p0 p1 can-move?]
+    {:post [(or (nil? %) (seq? %))]}
+    (loop [res      []
+           cur-pos  p0]
+      (if (= cur-pos p1) (seq res)
+        (let [can-move'?  #(and (can-move? %)
+                             (not (some #{%} res))
+                             (not= % p0))
+              neighbors'  (filter can-move'? (neighbors board cur-pos))
+              new-pos     (apply min-key (partial dist2 p1) neighbors')]
+          (recur (conj res new-pos) new-pos))))))
 
 (defn eval-moves
-  "Evaluates a sequence of blank `moves`, represented by coordinates
-  and starting from `blank-pos`.
-  Returns a triple of [newBoard newBlankPos numbers]
-  "
-  [n board moves blank-pos]
-  (reduce (fn [[cboard bp res] m]
-            [(move cboard (pos->idx n bp) (pos->idx n m))
-             m
-             (conj res (get-num n cboard m))])
-          [board blank-pos []] moves))
+  "For a sequence of `moves` (represented by positions of the blank)
+  returns a sequence of evaluated boards, starting from the initial `board`"
+  [board moves]
+  (rest (reductions (fn [board' pos] (move board' pos))
+          board moves)))
 
-(defn move-piece-to
-  [n board p1 piece]
-  (let [pb      (get-blank-pos n board)
-        cmove   #(not (top-left-edge? p1 %))
-        p0      (get-num-pos n board piece)
-        pmoves  (vec (moves n p0 p1 cmove))
-        mfn     (fn [[bpos cpos res] p]
-                  [cpos p
-                   (concat res
-                           (moves n bpos p
-                                  #(and (not= % cpos) (cmove %))) [cpos])])
-        [_ _ m] (reduce mfn [pb p0 []] pmoves)]
-    (eval-moves n board m pb)))
+(defn move-num-middle
+  "Finds a sequence of moves (blank positions) needed in order
+  for the number at position `p0` to get to the position `p1`.
+  `can-move?` is a predicate for the allowed cells.
+  Assumes that `p1` is neither on the right nor the bottom edge"
+  [board p0 p1 can-move?]
+  (let [moves (path board p0 p1 can-move?) ; number path
+        mfn   (fn [[res blank-pos last-pos] cur-pos]
+                [(concat
+                   res
+                   (path board blank-pos cur-pos
+                     #(and (not= % last-pos) (can-move? %)))
+                   [last-pos])
+                 last-pos cur-pos])
+        acc   (reduce mfn [[] (board :blank-pos) p0] moves)]
+    (first acc)))
 
-(defn move-piece
-  [n board p1 piece]
-  (let [[_ _ m] (move-piece-to n board p1 piece)] m))
+(defn move-num-redge
+  "Special case of moving a piece from [`x0`,`y0`] to [`x1`, `y1`] on right edge.
+  `can-move?` is a move masking predicate."
+  [board [x0 y0] [x1 y1] can-move?]
+  {:pre [= x0 (dec (board :n))]}
+  (let [below? (= y0 (inc y1))
+        p0     [x0 (if below? (inc y0) y0)]
+        m0     (if below? (move-num-middle board [x0 y0] p0 can-move?))
+        board' (if below? (last (eval-moves board m0)) board)]
+    (concat m0
+      (move-num-middle board' [(dec x1) y1] [x1 y1] can-move?)
+      (move-num-middle (assoc board' :blank-pos [(dec x1) y1])
+        p0 [x1 (inc y1)] can-move?)
+      [[(dec x1) (+ 2 y1)] [(dec x1) (inc y1)]
+       [(dec x1) y1] [x1 y1] [x1 (inc y1)]])))
 
-(defn move-piece-redge
-  "Special case of moving a piece into a position `p1` on right edge"
-  [n board [x y] piece]
-  (let
-    [[b1 h1 m1] (move-piece-to n board [x y] (dec piece))
-     [b2 h2 m2] (eval-moves n b1 [[(dec x) (dec y)]] h1)
-     [b3 h3 m3] (move-piece-to n b2 [x (inc y)] piece)
-     [_  _  m4] (eval-moves n b3 (moves n h3 [(dec x) y]) h3)]
-  (concat m1 m2 m3 m4 [(dec piece) piece])))
+(defn move-num-bedge
+  "Same as `move-num-redge, but for the bottom edge`"
+  [board p0 p1 can-move?]
+  {:pre [= (second p0) (dec (board :m))]}
+  (let [board'  (transpose-board board)
+        moves   (move-num-redge board' (swap p0) (swap p1)
+                  (comp can-move? swap))]
+    (map swap moves)))
+
+(defn move-num
+  "Moves correct number on a `board` to the target position `[x, y]`"
+  [board [x y] can-move?]
+  (let [idx    (pos->idx board [x y])
+        [n m]  [(board :n) (board :m)]
+        p0     (get-num-pos board (inc idx))
+        movefn (cond
+                 (= p0 [x y])  (constantly [])
+                 (and (= (inc x) n) (< y (- m 2))) move-num-redge
+                 (and (= (inc y) m) (< x (- n 2))) move-num-bedge
+                 :else         move-num-middle)]
+    (movefn board p0 [x y] can-move?)))
+
+(defn solve-order
+  "Returns sequence of positions to solve the board `n` by `m`"
+  ([n]
+    (apply concat
+      (for [k (range 0 (- n 1))]
+        (concat
+          (map #(vector % k) (range k n))
+          (map #(vector k %) (range (inc k) n))))))
+  ([n m]
+    {:pre [(> n 1) (> m 1)]}
+    (let [i0 (max 0 (- n m))
+          j0 (max 0 (- m n))]
+      (concat
+        (for [i (range 0 i0) j (range 0 m)] [i j])
+        (for [j (range 0 j0) i (range 0 n)] [i j])
+        (map (fn [[i j]] [(+ i i0) (+ j j0)])
+          (solve-order (min n m)))))))
+
+(defn top-left-edge?
+  "Returns true if position `[x y]` is in the masked left-top area,
+  (according to the board filling rule) relative to the pivot position `[px py]`.
+  The board is assumed to be square."
+  [[px py] [x y]]
+  (if (> py px)
+    (or (< x px) (<= y px) (and (= x px) (< y py)))
+    (or (< y py) (< x py) (and (= y py) (< x px)))))
 
 (defn solve
-  [n board]
-  [2 3 5 7])
+  "Returns sequence of moves (positions) needed in order to solve given `board`
+  using a greedy algorithm."
+  [board]
+  (let [[n m] [(board :n) (board :m)]
+        i0    (max 0 (- n m))
+        j0    (max 0 (- m n))
+        order (solve-order n m)]
+    (second (reduce
+      (fn [[board' res] [x y]]
+        (let [can-move? (fn [[i j]]
+                          (not (top-left-edge?
+                                 [(- x i0) (- y j0)]
+                                 [(- i i0) (- j j0)])))
+              moves     (move-num board' [x y] can-move?)
+              board''   (if (empty? moves) board'
+                          (last (eval-moves board' moves)))]
+          [board'' (concat res moves)]))
+      [board []]
+      order))))
+
+(defn solve-nums
+  "Same as `solve`, but returns a sequence of pieces' numbers"
+  [board]
+  (let [moves  (solve board)
+        boards (eval-moves board moves)]
+    (map get-num-at (conj boards board) moves)))
